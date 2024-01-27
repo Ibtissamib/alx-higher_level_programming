722b85de5a34e0199f4984875d900fb5d60227bb
Ibtissam <ibboutayeb@gmail.com>
2024-01-27 23:03:38 +0000

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 if __name__ == '__main__':
     import sys
     print('{:} {}{}'.format(
-        len(sys.argv) - 1,
-        'argument' if len(sys.argv) == 2 else 'arguments',
-        '.' if len(sys.argv) == 1 else ':'))
+    len(sys.argv) - 1,
+    'argument' if len(sys.argv) == 2 else 'arguments',
+    '.' if len(sys.argv) == 1 else ':'))
     for i in range(1, len(sys.argv)):
         print('{}: {}'.format(i, sys.argv[i]))
