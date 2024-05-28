02173d26fb3094b62f10f648dc83af8f06cb9f94
Ibtissam <ibboutayeb@gmail.com>
2024-05-28 22:02:37 +0000

    12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- lists all shows contained in hbtn_0d_tvshows
-SELECT title, genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id WHERE genre_id = NULL ORDER BY title ASC;
+SELECT title, genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id WHERE genre_id IS NULL ORDER BY title ASC;
