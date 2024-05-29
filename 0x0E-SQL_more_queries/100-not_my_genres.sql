3f84f41817d6a69d0ccc9a7495bc70b8569ead78
Ibtissam <ibboutayeb@gmail.com>
2024-05-29 01:04:05 +0000

    100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,5 +2,5 @@
  SELECT tv_genres.name
  FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
  LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
- WHERE title != "Dexter"
+ WHERE title != 'Dexter'
  ORDER BY name ASC;                            
