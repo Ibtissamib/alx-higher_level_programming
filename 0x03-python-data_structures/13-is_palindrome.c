ab77de0cd41bc89449af7fea60695927aaffeb5e
Ibtissam <ibboutayeb@gmail.com>
2024-06-05 00:45:34 +0000

    13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+  *is_palindrome-checks
+  *@head: listint_t **
+  *Return: int
+  */
+int is_palindrome(listint_t **head)
+{
+	listint_t *r = *head;
+
+	while (*head)
+	{
+		if ((*head)->n != r->n)
+			return (0);
+		*head = (*head)->next;
+		r = r->next;
+	}
+	return (1);
+}
+/**
+  **reverse_listint-reverses
+  *@head: listint_t **
+  *Return:  listint_t *
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = NULL;
+	listint_t *newnode = NULL;
+
+	if (*head == NULL)
+		return (NULL);
+	while (*head != NULL)
+	{
+		node = (*head)->next;
+		(*head)->next = newnode;
+		newnode = *head;
+		*head = node;
+	}
+	*head = newnode;
+	return (*head);
+}
